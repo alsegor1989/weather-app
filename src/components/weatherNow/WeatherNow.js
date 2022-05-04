c0818d33ed047252fc6f6f6b60ee5b14bc7908cf
Alexey Goryushin <alexey_goryushin@mail.ru>
2022-05-04 12:40:00 +0300

    weathernow bootstrap

--- src/components/weatherNow/WeatherNow.js
+++ src/components/weatherNow/WeatherNow.js
@@ -48,26 +48,28 @@ const WeatherNow = (props) => {
                     alt="Icon"
                     style={{ width: '200px' }}
                     className="mx-auto d-block" />
-                <div id="city">{`Город: ${data.name}`}</div>
-                <div id="date">{`Данные обновлены: ${dateString}`}</div>
-                <div id="temp">{`Температура: ${temp}`}</div>
-                <div id="feels_like">{`По ощущению: ${feels_like}`}</div>
-                <div id="pressure">{`Давление: ${Math.round(data.main.pressure / 1.333)} мм рт. ст.`}</div>
-                <div id="humidity">{`Влажность: ${data.main.humidity}%`}</div>
-                <div id="sky">{`Описание: ${data.weather[0].description}`}</div>
-                <div id="wind">
-                    <div id="speed">Ветер: {`${data.wind.speed}`} м/с</div>
-                    <Card.Img
-                        src={img}
-                        alt='Direction'
-                        style={{
-                            display: 'block', width: '30px', height: '30px', objectFit: 'contain',
-                            borderRadius: '50%', margin: '0 auto', transform: `rotate(${360 - data.wind.deg}deg)`
-                        }} />
-                    {/* <div id="direction">{`${data.wind.deg}`}</div> */}
-                </div>
-                <div id="sunrise">{`Восход: ${dateSunrise}`}</div>
-                <div id="sunset">{`Закат: ${dateSunset}`}</div>
+                <Card.Body>
+                    <Card.Title id="city">{`Погода сейчас: ${data.name}`}</Card.Title>
+                    <div id="updateTime">{`${dateString}`}</div>
+                    <div id="temp">{`Температура: ${temp}`}</div>
+                    <div id="feels_like">{`По ощущению: ${feels_like}`}</div>
+                    <divt id="pressure">{`Давление: ${Math.round(data.main.pressure / 1.333)} мм рт. ст.`}</divt>
+                    <div id="humidity">{`Влажность: ${data.main.humidity}%`}</div>
+                    <div id="sky">{`Описание: ${data.weather[0].description}`}</div>
+                    <div id="wind" style={{ display: 'flex' }}>
+                        <div id="speed">Ветер: {`${data.wind.speed}`} м/с</div>
+                        <Card.Img
+                            src={img}
+                            alt='Direction'
+                            style={{
+                                display: 'block', width: '25px', height: '25px', objectFit: 'contain',
+                                borderRadius: '50%', margin: '0 auto', transform: `rotate(${360 - data.wind.deg}deg)`
+                            }} />
+                        {/* <div id="direction">{`${data.wind.deg}`}</div> */}
+                    </div>
+                    <div id="sunrise">{`Восход: ${dateSunrise}`}</div>
+                    <div id="sunset">{`Закат: ${dateSunset}`}</div>
+                </Card.Body>
             </div>
         )
     }
